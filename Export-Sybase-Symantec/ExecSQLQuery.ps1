e470f4c63c1453c8ef600706675a3677ce00665c
Bruno Francisco Rodrigues <brunofranrodrigues@gmail.com>
2022-03-28 17:35:50 -0300

    Update ExecSQLQuery.ps1

--- Export-Sybase-Symantec/ExecSQLQuery.ps1
+++ Export-Sybase-Symantec/ExecSQLQuery.ps1
@@ -1,9 +1,9 @@
-﻿$SQLServer = "10.156.14.23,1433" #Server
-$DataBase = "uoldiveo-dw" #Database
-$table="fato_symantec" #Table
-$Username = "symantec" #User
+﻿$SQLServer = "192.168.2.1,1433" #Server
+$DataBase = "banco" #Database
+$table="tabela_symantec" #Table
+$Username = "usuario" #User
 $Password = "xxxxxx" #Password
-$Query = "SELECT * FROM fato_symantec;"
+$Query = "SELECT * FROM tabela_symantec;"
 $data = Invoke-SQLCMD -ServerInstance $SQLServer -Database $DataBase -Query $Query -Username $Username -Password $Password -Verbose
 #$i=0
 $data 
